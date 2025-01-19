dc2433e12cf05f635dfcae4d5f692bbf3bb9a1c0
TheBetterQwerty <166288160+TheBetterQwerty@users.noreply.github.com>
2025-01-20 00:11:04 +0530

    Update disk_encryptor.py

--- disk_encryptor.py
+++ disk_encryptor.py
@@ -4,28 +4,31 @@ import base64
 import os
 import argparse
 import hashlib
+import random 
+from getpass import getpass
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 
-def encrypt(key, text):
+def encrypt(key: bytes, text: str) -> str:
     iv = get_random_bytes(AES.block_size)
     cipher = AES.new(key,AES.MODE_CBC , iv)
     padded_text = pad(text, AES.block_size)
     encryption = base64.b64encode(cipher.encrypt(padded_text))
-    return encryption, iv
+    return iv + encryption
 
-def decrypt(key, encrypted_text, iv):
+def decrypt(key: bytes, encmsg: str) -> str:
     try:
+        iv = encmsg[:AES.block_size]
+        encmsg = encmsg[AES.block_size:]
         cipher = AES.new(key , AES.MODE_CBC , iv)
-        decrypted = cipher.decrypt(base64.b64decode(encrypted_text))
+        decrypted = cipher.decrypt(base64.b64decode(encmsg))
         decrypted_text = unpad(decrypted, AES.block_size)
         return decrypted_text
     except :
-        print("Wrong Password")
-        exit()
+        return 
 
-def traverse(drive):
+def traverse(drive: str) -> list:
     req_files = []
     for root,_,files in os.walk(drive):
         for file_name in files:
@@ -33,51 +36,76 @@ def traverse(drive):
             req_files.append(file_path)
     return req_files
 
-def encryptor(file , key):
-    with open(file , "rb") as f:
-        file_contents = f.read()
+def encryptor(file: str, key: bytes) -> bool:
+    try:
+        with open(file , "rb") as f:
+            file_contents = f.read()
+    
+        time = random.randint(1,9)
 
-    encrypted_file_contents, iv = encrypt(key , file_contents)
+        for i in range(time):
+            file_contents = encrypt(key , file_contents)
 
-    with open(file , "wb") as f:
-        f.write(iv + encrypted_file_contents)
+        with open(file , "wb") as f:
+            f.write(file_contents + str(time).encode())
+    except Exception as err:
+        print(f"[!] Error {err}")
+        return False
+    else:
+        return True
 
-def decryptor(file , key):
-    with open(file , "rb") as f:
-        file_contents = f.read()
-    iv = file_contents[:AES.block_size]
-    file_contents = file_contents[AES.block_size:]
-    decrypted_contents = decrypt(key, file_contents, iv)
+def decryptor(file , key) -> bool:
+    try:
+        with open(file , "rb") as f:
+            file_contents = f.read()
 
-    with open(file , "wb") as f:
-        f.write(decrypted_contents)
+        time = int(chr(file_contents[-1]))
+        file_contents = file_contents[:-1]
+        for i in range(time):
+            file_contents = decrypt(key, file_contents)
+            if not file_contents:
+                print(f"[!] Decryption failed for {file}")
+                return False
+            
+        with open(file , "wb") as f:
+            f.write(file_contents)
+    except Exception as err:
+        print(f"[!] Error: {err}")
+        return False
+    else:
+        return True
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description=" *** Script To Encrypt Or Decrypt a Drive ***")
-    parser.add_argument("--encrypt", type=str , help="Encrypts the drive")
-    parser.add_argument("--decrypt" , type=str ,help="Decrypts the drive")
-    parser.add_argument("-p", "--password", type=str , required=True , help="Password")
+    parser.add_argument("-e", "--encrypt", type=str , help="Encrypts the drive")
+    parser.add_argument("-d", "--decrypt" , type=str ,help="Decrypts the drive")
     args = parser.parse_args()
     
-    if args.encrypt and args.decrypt:
-        print("Cannot Encrypt and Decrypt same time")
-        exit(1)
+    if (args.encrypt and args.decrypt) or (not args.encrypt and not args.decrypt):
+        print("[!] Please Specify a valid Operation! Press -h for help")
+        return
 
-    password = args.password
+    password: str = getpass("[?] Enter a password: ")
+    _passwrd: str = getpass("[?] Enter password again: ")
+
+    if not password == _passwrd:
+        print("[!] Passwords don't match. Try again...")
+        return
+
     key = hashlib.sha256(password.encode()).digest()
     if not os.path.exists(args.encrypt or args.decrypt):
-        print("Path Doesnt Exists")
-        exit(1)
+        print("[!] Path Doesnt Exists")
+        return
 
-    files_found = traverse(args.encrypt or args.decrypt)
+    files_found: list = traverse(args.encrypt or args.decrypt)
     if args.encrypt:
         for file in files_found:
-            encryptor(file , key)
-            print(f"Encrypted -> {file}")
+            if encryptor(file , key):
+                print(f"[#] Encrypted -> {file}")
     if args.decrypt:
         for file in files_found:
-            decryptor(file, key)
-            print(f"Decrypted -> {file}")
+            if decryptor(file, key):
+                print(f"[$] Decrypted -> {file}")
 
 if __name__ == "__main__":
     main()
